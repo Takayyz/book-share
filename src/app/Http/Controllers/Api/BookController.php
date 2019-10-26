12eb8e7de48efa6c8c1cb7234d221441b5f6ad79
osakana11011 <taka.knk.11011@gmail.com>
2019-10-26 15:46:03 +0900

    本詳細取得API実装

--- src/app/Http/Controllers/Api/BookController.php
+++ src/app/Http/Controllers/Api/BookController.php
@@ -16,17 +16,12 @@ class BookController extends Controller
         $page    = $request->input('page') ?? 1;
         $hits    = $request->input('hits') ?? 1;
 
-        if (!empty($keyword)) {
-            return response()->json([
-                'status' => '200',
-                'books'  => $books,
-            ]);
-        } else {
-            return response()->json([
-                'status' => '400',
-                'books'  => [],
-            ]);
-        }
+        return $this->getBooks($keyword, $page, $hits);
+    }
+
+    public function showDetails($isbn)
+    {
+        return $this->getBookDetails($isbn);
     }
 
     private function getBooks($keyword, $page, $hits)
@@ -34,18 +29,59 @@ class BookController extends Controller
         $applicationId = config('rakutenApi.APPLICATION_ID');
         $url = self::RAKUTEN_API_URL.'?applicationId='.$applicationId.'&keyword='.$keyword.'&hits='.$hits.'&page='.$page;
 
-        $data = json_decode(file_get_contents($url));
+        $response = @file_get_contents($url);
 
-        $books = array_map(function ($book) {
-            $item = $book->Item;
+        if ($response) {
+            $data = json_decode($response);
+            $books = array_map(function ($book) {
+                $item = $book->Item;
+                return [
+                    'isbn_code' => $item->isbn,
+                    'title'     => $item->title,
+                    'author'    => $item->author,
+                    'image_url' => $item->largeImageUrl,
+                ];
+            }, $data->Items);
+
             return [
+                'status' => '200',
+                'books'  => $books,
+            ];
+        } else {
+            return [
+                'status' => '400',
+                'books'  => [],
+            ];
+        }
+    }
+
+    private function getBookDetails($isbn)
+    {
+        $applicationId = config('rakutenApi.APPLICATION_ID');
+        $url = self::RAKUTEN_API_URL.'?applicationId='.$applicationId.'&isbnjan='.$isbn;
+
+        $response = @file_get_contents($url);
+
+        if ($response) {
+            $data = json_decode($response);
+
+            $item = $data->Items[0]->Item;
+            $book = [
                 'isbn_code' => $item->isbn,
                 'title'     => $item->title,
                 'author'    => $item->author,
                 'image_url' => $item->largeImageUrl,
             ];
-        }, $data->Items);
 
-        return $books;
+            return [
+                'status' => '200',
+                'book'  => $book,
+            ];
+        } else {
+            return [
+                'status' => '400',
+                'book'  => [],
+            ];
+        }
     }
 }
