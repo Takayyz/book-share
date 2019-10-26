e1dfa6d1db400dcef6246bdd80f612286f3d6aa4
osakana11011 <taka.knk.11011@gmail.com>
2019-10-26 13:54:00 +0900

    本一覧APIの実装

--- src/app/Http/Controllers/Api/BookController.php
+++ src/app/Http/Controllers/Api/BookController.php
@@ -8,8 +8,26 @@ use App\Http\Controllers\Controller;
 class BookController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json(['apple' => 'red', 'peach' => 'pink']);
+        $keyword = $request->input('keyword');
+        $page    = $request->input('page');
+        $hits    = $request->input('hits');
+
+        $url = 'https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?applicationId=1019399324990976605&keyword='.$keyword.'&hits='.$hits.'&page='.$page;
+
+        $data = json_decode(file_get_contents($url));
+
+        $books = array_map(function ($book) {
+            $item = $book->Item;
+            return [
+                'isbn_code' => $item->isbn,
+                'title'     => $item->title,
+                'author'    => $item->author,
+                'image_url' => $item->largeImageUrl,
+            ];
+        }, $data->Items);
+
+        return response()->json($books);
     }
 }
